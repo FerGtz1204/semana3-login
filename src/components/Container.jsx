e45b9a6b86687b4cbfdec4bff5b78fe0435b4729
FerGtz1204 <fernanda_gtz@hotmail.com>
2024-09-17 12:51:45 -0600

    Update Container.jsx

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,19 +1,10 @@
 import React, { useState } from "react";
 
-import LoginForm from "./LoginForm";
 import TaskList from "./TaskList";
 import TodoForm from "./TodoForm";
 
 const Container = () => {
   const [list, setList] = useState([]);
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  const [userToken, setUserToken] = useState(''); // Para almacenar el token
-
-  const handleLogin = (token) => {
-      setUserToken(token); // Guarda el token recibido
-      setLoggedIn(true); // Cambia el estado a "logueado"
-  };
 
   const handleAddItem = addItem => {
     setList([...list, addItem]);
